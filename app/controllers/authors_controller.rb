682465de1cc813d7edae28fbf9cc90b8cc085f94
Bo Brockmann <bbrockmann1@gmail.com>
2022-11-26 17:12:28 -0700

    commit tree 1b68add059b3a595783e077742adcce39b44702a

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,15 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invaid_params_response
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+
   end
 
   private
@@ -17,5 +17,8 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
-  
+
+  def render_invaid_params_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end  
 end
